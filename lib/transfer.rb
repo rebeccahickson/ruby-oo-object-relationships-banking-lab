db0b3f872eabe462b77ad992221f10206495d8da
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-05 14:58:48 -0500

    commit tree 9c7c3b5e12a44961d05088488826354a2f28f244

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && @status == 'pending' && sender.balance >= amount
+      @status = 'complete'
+      receiver.deposit(amount)
+      sender.withdrawal(amount)
+    else
+      @status = 'rejected'
+      'Transaction rejected. Please check your account balance.'
+    end
+  end
+
+  def reverse_transfer
+    if @status == 'complete'
+      receiver.withdrawal(amount)
+      sender.deposit(amount)
+      @status = 'reversed'
+    end
+  end
 end
